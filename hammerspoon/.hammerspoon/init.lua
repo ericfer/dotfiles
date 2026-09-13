8f9bf03371c5eed9e7163fbfbf93baffb635bcc1
Eric Fer <eric.fer@gmail.com>
2026-09-13 15:59:22 -0300

    creating a hammerspoon config for global keybinds

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,39 @@
+hs.hotkey.bind({"alt"}, "s", function()
+    hs.application.launchOrFocus("Sublime Text")
+end)
+
+hs.hotkey.bind({"alt"}, "i", function()
+    hs.application.launchOrFocus("Calendar")
+end)
+
+hs.hotkey.bind({"alt"}, "c", function()
+    hs.application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bind({"alt"}, "e", function()
+    hs.application.launchOrFocus("Evernote")
+end)
+
+hs.hotkey.bind({"alt"}, "d", function()
+    hs.application.launchOrFocus("Finder")
+end)
+
+hs.hotkey.bind({"alt"}, "p", function()
+    hs.application.launchOrFocus("Preview")
+end)
+
+hs.hotkey.bind({"alt"}, "v", function()
+    hs.application.launchOrFocus("Visual Studio Code")
+end)
+
+hs.hotkey.bind({"alt"}, "t", function()
+    hs.application.launchOrFocus("iTerm")
+end)
+
+hs.hotkey.bind({"alt"}, "w", function()
+    hs.urlevent.openURL("https://www.youtube.com/playlist?list=WL")
+end)
+
+hs.hotkey.bind({"alt"}, "z", function()
+    hs.urlevent.openURL("https://www.linkedin.com/in/ericfer/")
+end)
